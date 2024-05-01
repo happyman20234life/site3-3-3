2a617756dd27a78564eb73abd2f3ed3c528884f9
Dan Truong <146357532+happyman20234life@users.noreply.github.com>
2024-05-01 08:37:50 -0400

    Add files via upload

---
+++ Argentine/gamejavascript.js/Ts2.js
@@ -0,0 +1,27 @@
+function openEmbed() {
+
+    var newWindow = window.open('about:blank');
+
+
+    var iframe = document.createElement('iframe');
+
+
+    iframe.src = 'https://happyman20234life.github.io/Collection-of-games/TS2/';
+
+
+    iframe.style.position = 'absolute';
+    iframe.style.top = '0';
+    iframe.style.left = '0';
+    iframe.style.width = '100%';
+    iframe.style.height = '100%';
+    iframe.style.border = 'none';
+
+
+    newWindow.document.body.appendChild(iframe);
+
+
+    newWindow.document.title = 'Argentine | Tiny Fishing';
+}
+
+
+document.getElementById('ts2').addEventListener('click', openEmbed);
